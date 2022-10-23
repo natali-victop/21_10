9d4e74c375ee82594f27c5da11444ee2b5394c55
natali-victop <115889479+natali-victop@users.noreply.github.com>
2022-10-24 01:55:36 +0300

    Add files via upload

---
+++ task1/js/main.js
@@ -0,0 +1,46 @@
+// console.log("Hello world!!!");
+// var lesson = "Учим JAVAScript";
+// console.log(lesson);
+// let userName = "Иван";
+// console.log(userName);
+// const birtYear = 1990
+// console.log(birtYear)
+
+
+
+
+let personAge = prompt('Сколько тебе лет?', '15');
+let personGender = confirm('Ты мальчик?');
+if(personGender){
+    alert('Эй, парень, столько тебе '+ personAge ='!);
+}
+
+
+// for (let i = 0; i <10; i++){
+// console.log('i =' +i);
+// }
+
+// for (let i=2; i<=10; i++){
+// if(i%2 ==0){
+//     console.log('i = ' +i)');
+// }
+
+// }
+// 
+
+
+
+// let grouAge = [12, 15, 14];
+// console.log(grouAge);
+// let sum = 0;
+// for(let i = 0; i<grouAge.length; i++){
+//     sun=sun + grouAge[i];
+// console.log(sum);
+
+// }
+
+// console.log('Сумма лет равна: ' + sun);
+// let averAge = sun / grouAge.length;
+// console.log('Chtlybq djphfcn uheggs' + averAge)
+
+// const fruits = ['Яблоко']
